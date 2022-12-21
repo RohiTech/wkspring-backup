31d646cfa161a1ff8f9619f9eb2b6edfad88bee8
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2022-12-21 00:08:17 -0600

    Migrating from WebSecurityConfigureAdapter

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -1,17 +1,19 @@
 package com.appsdeveloperblog.app.ws.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 // import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.appsdeveloperblog.app.ws.service.UserService;
 
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter
+public class WebSecurity //extends WebSecurityConfigurerAdapter
 {
 	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -22,6 +24,25 @@ public class WebSecurity extends WebSecurityConfigurerAdapter
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 	
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception
+	{
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder =
+				http.getSharedObject(AuthenticationManagerBuilder.class);
+		
+		authenticationManagerBuilder
+		.userDetailsService(userDetailsService)
+		.passwordEncoder(bCryptPasswordEncoder);
+		
+		http.csrf().disable()
+		.authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
+		.anyRequest().authenticated();
+		
+		return http.build();
+	}
+	
+	/*
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
@@ -36,4 +57,5 @@ public class WebSecurity extends WebSecurityConfigurerAdapter
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
+	*/
 }
