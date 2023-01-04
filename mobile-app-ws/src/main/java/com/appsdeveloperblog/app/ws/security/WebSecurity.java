a3014f0fb06e9c1f4f92d2fa1b92fdb327bafb5e
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2023-01-04 14:20:36 -0600

    Implementing SecurityConstants class

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -36,7 +36,7 @@ public class WebSecurity //extends WebSecurityConfigurerAdapter
 		.passwordEncoder(bCryptPasswordEncoder);
 		
 		http.csrf().disable()
-		.authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll()
+		.authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
 		.anyRequest().authenticated();
 		
 		return http.build();
