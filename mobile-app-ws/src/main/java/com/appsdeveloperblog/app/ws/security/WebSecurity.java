36cec3f19e37339496e1fe4f29b1a24e54f143d6
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2023-01-04 16:53:50 -0600

    Implementing Authentication Filter

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -2,10 +2,11 @@ package com.appsdeveloperblog.app.ws.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 // import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import com.appsdeveloperblog.app.ws.service.UserService;
 
 @EnableWebSecurity
-public class WebSecurity //extends WebSecurityConfigurerAdapter
+public class WebSecurity extends WebSecurityConfigurerAdapter
 {
 	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -24,6 +25,20 @@ public class WebSecurity //extends WebSecurityConfigurerAdapter
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+		.permitAll()
+		.anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	/*
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception
 	{
@@ -41,6 +56,7 @@ public class WebSecurity //extends WebSecurityConfigurerAdapter
 		
 		return http.build();
 	}
+	*/
 	
 	/*
 	@Override
